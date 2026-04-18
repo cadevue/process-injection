eeecc3392700af0b84ede8cef71f6d307543f3dd
Addin Munawwar 🌧 <91366027+cadevue@users.noreply.github.com>
2026-04-19 01:55:23 +0700

    feat: get_pid_by_name utils in common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,7 @@
 // Expose Module
+pub mod process;
 pub mod raii;
 
 // Module Re-export
+pub use process::*;
 pub use raii::*;
---
+++ common/src/process.rs
@@ -0,0 +1,40 @@
+use std::{ffi::OsString, os::windows::ffi::OsStringExt};
+
+use windows_sys::Win32::System::Diagnostics::ToolHelp::{
+    CreateToolhelp32Snapshot, PROCESSENTRY32W, Process32FirstW, Process32NextW, TH32CS_SNAPPROCESS,
+};
+
+use crate::OwnedHandle;
+
+/// Traversing process snapshot and find pid for a specified process name.
+/// https://learn.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
+pub fn find_pid_by_name(name: &str) -> Option<u32> {
+    let raw_snapshot = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+    let snapshot = OwnedHandle::new(raw_snapshot)?;
+
+    let mut pe32: PROCESSENTRY32W = unsafe { std::mem::zeroed() };
+    pe32.dwSize = size_of::<PROCESSENTRY32W>() as u32;
+
+    if unsafe { Process32FirstW(snapshot.as_raw(), &mut pe32) } == 0 {
+        return None;
+    }
+
+    loop {
+        let len = pe32
+            .szExeFile
+            .iter()
+            .position(|&c| c == 0)
+            .unwrap_or(pe32.szExeFile.len());
+        let pname_wide = &pe32.szExeFile[..len];
+        let pname_os = OsString::from_wide(pname_wide);
+        let pname = pname_os.to_string_lossy();
+
+        if pname.eq_ignore_ascii_case(name) {
+            return Some(pe32.th32ProcessID);
+        }
+
+        if unsafe { Process32NextW(snapshot.as_raw(), &mut pe32) } == 0 {
+            return None;
+        }
+    }
+}
