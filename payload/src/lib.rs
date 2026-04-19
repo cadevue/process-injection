d17a975d47ef8389db4f9a8eb32be8f016330465
Addin Munawwar 🌧 <91366027+cadevue@users.noreply.github.com>
2026-04-19 13:46:52 +0700

    feat: MessageBox payload

--- payload/src/lib.rs
+++ payload/src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::ffi::c_void;
+use std::ptr::null_mut;
+
+use common::OwnedHandle;
+use windows_sys::Win32::Foundation::{HANDLE, HINSTANCE, TRUE};
+use windows_sys::Win32::System::Threading::CreateThread;
+
+use windows_sys::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
+use windows_sys::Win32::UI::WindowsAndMessaging::{MB_ICONWARNING, MB_OK, MessageBoxW};
+use windows_sys::core::BOOL;
+
+#[unsafe(no_mangle)]
+#[allow(non_snake_case)]
+extern "system" fn DllMain(_: HINSTANCE, call_reason: u32, _: *mut c_void) -> BOOL {
+    if call_reason == DLL_PROCESS_ATTACH {
+        let h: HANDLE = unsafe {
+            CreateThread(
+                null_mut(),
+                0,
+                Some(attach_routine),
+                null_mut(),
+                0,
+                null_mut(),
+            )
+        };
+        let _ = OwnedHandle::new(h); // Automatic cleanup
+    }
+
+    TRUE
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+unsafe extern "system" fn attach_routine(_: *mut c_void) -> u32 {
+    let title: Vec<u16> = "Warning".encode_utf16().chain(Some(0)).collect();
+    let message: Vec<u16> = "Your spaceship has been hijacked!"
+        .encode_utf16()
+        .chain(Some(0))
+        .collect();
+    unsafe {
+        // Create a message box
+        MessageBoxW(
+            std::ptr::null_mut(),
+            message.as_ptr(),
+            title.as_ptr(),
+            MB_OK | MB_ICONWARNING,
+        );
+    };
+    0
 }
